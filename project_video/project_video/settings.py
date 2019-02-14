73577d29f7f0fdb493e5db8d5c638c0a6d999b37
luisacatherine <luisa@alphatech.id>
2019-02-14 23:05:17 +0700

    Login

--- project_video/project_video/settings.py
+++ project_video/project_video/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home_app',
+    'signup',
     'widget_tweaks'
 ]
 
@@ -125,3 +126,5 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+LOGIN_REDIRECT_URL = 'home_app'
+LOGOUT_REDIRECT_URL = 'home_app'
