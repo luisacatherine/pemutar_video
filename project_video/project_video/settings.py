55b75b0d5eb257ca99302fb885766390ec6d0e5d
luisacatherine <luisa@alphatech.id>
2019-02-14 18:47:17 +0700

    Video home dan detail

--- project_video/project_video/settings.py
+++ project_video/project_video/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home_app'
+    'home_app',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
